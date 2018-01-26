5f3ad58c8336ac38a48ddf02fdae476e8886be3c
Sean Oughton <seanoughton@gmail.com>
2018-01-26 00:26:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,5 +26,4 @@ def printer(attendees)
   assign_rooms(attendees).each do |x|
     puts x
   end
-  #Hint: Remember that methods can call other methods. If the return value of assign_rooms is an array of room assignments, how can you print out each assignment? You'll need to iterate over your array of room assignments in order to puts out each individual assignment.
 end
