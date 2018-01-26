627649f2a25e0747a5657dfe96996ee0536e5fe4
Sean Oughton <seanoughton@gmail.com>
2018-01-26 00:18:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.collect do|attendee,index|
-    room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
   #Hint: Remember that the return value of the each method is the original array that you are calling it on. How can you collect or store the room assignment strings you are creating as you iterate and return them at the end of your assign_rooms method? Google or use Ruby Docs to find the correct method.
 end
