bc00234135366d49a9617237c26695bbfa92a920
Sean Oughton <seanoughton@gmail.com>
2018-01-26 00:11:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,5 @@ def assign_rooms(attendees)
     room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
   #Hint: Remember that the return value of the each method is the original array that you are calling it on. How can you collect or store the room assignment strings you are creating as you iterate and return them at the end of your assign_rooms method? Google or use Ruby Docs to find the correct method.
-  #return a list of room assignments in the form of: "Hello, _____! You'll be assigned to room _____!"
   return room_assignments
 end
