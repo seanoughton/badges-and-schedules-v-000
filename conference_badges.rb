227999bc91d8638f86a960384b56e84fb2d4244f
Sean Oughton <seanoughton@gmail.com>
2018-01-26 00:16:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do|attendee,index|
+  attendees.each_with_index.collect do|attendee,index|
     room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
   #Hint: Remember that the return value of the each method is the original array that you are calling it on. How can you collect or store the room assignment strings you are creating as you iterate and return them at the end of your assign_rooms method? Google or use Ruby Docs to find the correct method.
